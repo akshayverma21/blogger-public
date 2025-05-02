c1e72b8146843077e3c787ae487b8e8857fe110d
Akshay Verma <vakshay.523@gmail.com>
2025-05-02 23:10:57 +0530

    Update urls.py

--- Blog/urls.py
+++ Blog/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('akshay-superadmin/', admin.site.urls),
     path('', include('Kahaniya.urls')),
     path('accounts/', include('allauth.urls')),
 ]
